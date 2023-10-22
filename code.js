d0fadb7dff4faca249f7e19c045de875ae93b686
zLumoo <lucasmottta2004silva@gmail.com>
2023-10-21 22:52:15 -0300

    Atualiza final 1.4

--- code.js
+++ code.js
@@ -1,21 +1,24 @@
-var dataTermino = new Date("2023-10-22T09:59:59").getTime();
+// Definindo a data desejada no horário GMT-2
+const dataDesejada = new Date(Date.UTC(2023, 9, 22, 16, 0, 0)); // Mês 9 representa outubro, pois janeiro é o mês 0
+
+// Atualiza o temporizador a cada segundo
+const temporizador = setInterval(() => {
+    // Obtendo a data e hora atuais em milissegundos
+    const agora = new Date().getTime();
     
-        var x = setInterval(function() {
-    
-            var dataAtual = new Date().getTime();
-    
-            var diferenca = dataTermino - dataAtual;
-    
-            var dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
-            var horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
-            var segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
-    
-            document.getElementById("temporizador").innerHTML = dias + "d " + horas + "h "
-            + minutos + "m " + segundos + "s ";
-    
-            if (diferenca < 0) {
-                clearInterval(x);
-                document.getElementById("temporizador").innerHTML = "UPDATE RELEASED";
-            }
-        }, 1000);
+    const diferenca = dataDesejada - agora;
+
+    // Cálculos para dias, horas, minutos e segundos
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
+
+    document.getElementById('temporizador').innerHTML = `${dias}d ${horas}h ${minutos}m ${segundos}s`;
+
+    // Expired time
+    if (diferenca < 0) {
+        clearInterval(temporizador);
+        document.getElementById('temporizador').innerHTML = 'UPDATE RELEASED';
+    }
+}, 1000); // 1000 milissegundos = 1 segundo

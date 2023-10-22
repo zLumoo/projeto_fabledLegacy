e4fa39058dc83eb8b663d3c0d3257ca712f35b20
zLumoo <lucasmottta2004silva@gmail.com>
2023-10-22 09:54:57 -0300

    Atualização final 1.5

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
-// Definindo a data desejada no horário GMT-2
-const dataDesejada = new Date(Date.UTC(2023, 9, 22, 16, 0, 0)); // Mês 9 representa outubro, pois janeiro é o mês 0
+// Definindo a data desejada no horário de Brasília (GMT-3)
+const dataDesejada = new Date(Date.UTC(2023, 9, 22, 10, 0, 0)); // Mês 9 representa outubro, pois janeiro é o mês 0
 
 // Atualiza o temporizador a cada segundo
 const temporizador = setInterval(() => {
     // Obtendo a data e hora atuais em milissegundos
-    const agora = new Date().getTime();
-    
+    const agora = new Date().getTime() - 3 * 60 * 60 * 1000; // Ajuste para o horário de Brasília (GMT-3)
+
     const diferenca = dataDesejada - agora;
 
     // Cálculos para dias, horas, minutos e segundos
@@ -16,7 +16,7 @@ const temporizador = setInterval(() => {
 
     document.getElementById('temporizador').innerHTML = `${dias}d ${horas}h ${minutos}m ${segundos}s`;
 
-    // Expired time
+    // Tempo expirado
     if (diferenca < 0) {
         clearInterval(temporizador);
         document.getElementById('temporizador').innerHTML = 'UPDATE RELEASED';
